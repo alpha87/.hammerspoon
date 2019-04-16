6762015f13bfd9a1b5268650d32428bd51902f2d
alpha87 <jianxun2004@gmail.com>
2019-04-16 17:43:20 +0800

    可自定义刷新时间和标题数量，增加了logo

--- sspai/sspai.lua
+++ sspai/sspai.lua
@@ -2,11 +2,17 @@
     少数派最新文章获取，点击后打开对应文章。
 --]]
 
+-- 请求文章数量
+local article_num = "5"
+
+-- 刷新时间（秒）
+local s = 18000
+
 local menubar = hs.menubar.new()
 local title_items = {}
 local articleKey = {}
 local headers = {}
-local articlesApi = "https://sspai.com/api/v1/articles?offset=0&limit=5&type=recommend_to_home&sort=recommend_to_home_at&include_total=false"
+local articlesApi = "https://sspai.com/api/v1/articles?offset=0&limit=" .. article_num .. "&type=recommend_to_home&sort=recommend_to_home_at&include_total=false"
 headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
 
 function updateMenubar()
@@ -29,7 +35,8 @@ function getArticles()
                 return 
             end
             raw_json = hs.json.decode(body)
-            menubar:setTitle("少数派")
+            -- menubar:setTitle("少数派")
+            menubar:setIcon(hs.image.imageFromPath("/Users/lijianxun/.hammerspoon/sspai/icon.png"):setSize({w=16,h=16}))
             title_items = {}
 
             for k, v in pairs(raw_json.list) do
@@ -44,6 +51,6 @@ function getArticles()
     )
 end
 
-menubar:setTitle("加载中...")
 getArticles()
 updateMenubar()
+hs.timer.doEvery(s, getArticles)
